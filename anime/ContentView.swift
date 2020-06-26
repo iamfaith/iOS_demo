10409ab78215ab1fbb4eafe6da7208428fbd5e53
xianzixiang <xianzixiang@wps.cn>
2020-06-25 23:56:35 -0400

    Initial Commit

---
+++ anime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  anime
+//
+//  Created by faith on 2020/6/25.
+//  Copyright © 2020 iamfaith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
